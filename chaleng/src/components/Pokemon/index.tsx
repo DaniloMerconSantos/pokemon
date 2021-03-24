b6794b8b831e6e821174023bca3b7e9e1c8c1f9e
Danilo Mercon <dsantos@eaibrasil.com.br>
2021-03-24 11:31:13 -0300

    wip

---
+++ chaleng/src/components/Pokemon/index.tsx
@@ -0,0 +1,12 @@
+import react, { Component } from 'react';
+
+class Pokemon extends Component {
+    
+    render(){
+        return (
+            <h1>pokemon</h1>
+        )
+    }
+}
+
+export default Pokemon;
